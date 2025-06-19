2268efd0248ebf351130700f928a442719094f2b
Viswa0123 <viswanath0889@gmail.com>
2025-06-19 18:04:00 +0530

    New Modules Added

--- EMS/src/Main.java
+++ EMS/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("**************** Welcome To Employee Management System ****************");
-        int n = 0;
+        int n;
         Scanner in = new Scanner(System.in);
         do {
             System.out.println(
